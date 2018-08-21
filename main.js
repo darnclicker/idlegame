90181c270ea73efe7d33db49d434d066d261797e
Darren <darren.mcdonagh@voxfp.com>
2018-08-21 16:53:54 +0100

    Updated game engine

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var cookies = 0;
 
-function cookieClick(number) {
-    cookies = cookies + number;
+function cookieClick() {
+    cookies = cookies+ 1;
     document.getElementById("cookies").innerHTML = cookies;
+};
+
+function updateGame(cookies_per_second) {
+  cookies = cookies + cookies_per_second;
+  document.getElementById("cookies").innerHTML = cookies;
 };
 
 var cursors = 0;
@@ -12,7 +17,7 @@ function buyCursor(){
     var cursorCost = Math.floor(15 * Math.pow(1.1,cursors));     //works out the cost of this cursor
     if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
         cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
+    	  cookies = cookies - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
         document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
     };
@@ -21,20 +26,20 @@ function buyCursor(){
 };
 
 function buyGrandma(){
-    var grandmaCost = Math.floor(50 * Math.pow(1.3,grandmas));     //works out the cost of this grandma
-    if(cookies >= grandmaCost){                                   //checks that the player can afford the grandma
-        grandmas = grandmas + 1;                                   //increases number of grandmas
-    	cookies = cookies - grandmaCost;                          //removes the cookies spent
-        document.getElementById('grandmas').innerHTML = grandmas;  //updates the number of grandmas for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    var grandmaCost = Math.floor(50 * Math.pow(1.3,grandmas));
+    if(cookies >= grandmaCost){
+        grandmas = grandmas + 1;
+    	cookies = cookies - grandmaCost;
+        document.getElementById('grandmas').innerHTML = grandmas;
+        document.getElementById('cookies').innerHTML = cookies;
     };
-    var nextCost = Math.floor(50 * Math.pow(1.3,grandmas));       //works out the cost of the next grandma
-    document.getElementById('grandmaCost').innerHTML = nextCost;  //updates the grandma cost for the user
+    var nextCost = Math.floor(50 * Math.pow(1.3,grandmas));
+    document.getElementById('grandmaCost').innerHTML = nextCost;
 };
 
 window.setInterval(function(){
 
   amount_of_cookies_to_add = cursors + (3*grandmas);
-  cookieClick(amount_of_cookies_to_add);
+  updateGame(amount_of_cookies_to_add);
 
 }, 1000);
